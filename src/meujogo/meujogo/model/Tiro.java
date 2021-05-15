16c54a5985fc9af2e986112149ca2783b170436b
Andrew <andrewdw18@gmail.com>
2021-05-15 15:00:40 -0300

    ## Tiro - Ajustar de onde sai o Tiro dependendo da imagem; - Ideia de definir outros tiros; - procurar Imagens para isso ou fazer uma;

--- src/meujogo/meujogo/model/Tiro.java
+++ src/meujogo/meujogo/model/Tiro.java
@@ -1,6 +1,10 @@
+
 package meujogo.model;
 
-import java.awt.*;
+import java.awt.Image;
+import java.awt.Rectangle;
+
+import javax.swing.ImageIcon;
 
 public class Tiro {
     private Image imagem;
@@ -8,18 +12,63 @@ public class Tiro {
     private int largura, altura;
     private boolean isVisivel;
 
-    private static final int LARGURA = 938;
-    private static int VELOCIDADE = 2;
+    private static final int LARGURA = 938; // onde o tiro some
+    private static int VELOCIDADE = 9; // Velocidade do Tiro
 
     public Tiro(int x, int y) {
         this.x = x;
-        this.y = y;
+        this.y = y + 15;
         isVisivel = true;
 
     }
 
     public void load() {
+        ImageIcon ref = new ImageIcon("res\\tiro_vermelho.png");
+        imagem = ref.getImage();
 
+        this.largura = imagem.getWidth(null);
+        this.altura = imagem.getHeight(null);
+
+    }
+
+    public void update() {
+        this.x += VELOCIDADE;
+
+        if (this.x > LARGURA) {
+            isVisivel = false;
+        }
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, largura, altura);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isVisivel() {
+        return isVisivel;
+    }
+
+    public void setVisivel(boolean isVisivel) {
+        this.isVisivel = isVisivel;
+    }
+
+    public static int getVELOCIDADE() {
+        return VELOCIDADE;
+    }
+
+    public static void setVELOCIDADE(int vELOCIDADE) {
+        VELOCIDADE = vELOCIDADE;
+    }
+
+    public Image getImagem() {
+        return imagem;
     }
 
 }
